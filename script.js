20590435519349f26b164d4bb08f8ea9825a7b75
Matteo <matteo.fiorini@rocketmail.com>
2024-11-20 16:50:56 +0000

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-let button = document.getElementById('play')
-
-function handleButton() {
-    let playerOne = document.getElementById('player-one').value
-    let playerTwo = document.getElementById('player-two').value
+function play() {
+    for (let i = 0; i < 3; i++) {        
+        let playerOne = prompt("Choose one: rock, paper or scissors?")
+        let computerChoice = getComputerChoice()
     
-    if (playerOne === playerTwo) alert("It's a draw!")
-    else if (
-        (playerOne === 'rock' && playerTwo === 'scissors') || 
-        (playerOne === 'scissors' && playerTwo === 'paper') ||
-        (playerOne === 'paper' && playerTwo === 'rock')
-    ) alert("Player 1 won!")
-    else alert("Player 2 won!")
+        if (playerOne === computerChoice) alert("Computer chose " + computerChoice + ". It's a draw!")
+        else if (
+            (playerOne === 'rock' && computerChoice === 'scissors') || 
+            (playerOne === 'scissors' && computerChoice === 'paper') ||
+            (playerOne === 'paper' && computerChoice === 'rock')
+        ) {
+            alert("Computer chose " + computerChoice + ". Player 1 won!")
+            break
+        } 
+        else alert("Computer chose " + computerChoice + ". Computer won!")
+    }
 }
 
-button.addEventListener('click', handleButton)
+function getComputerChoice() {
+    let options = ['rock', 'paper', 'scissors']
+    let rnd = Math.floor(Math.random() * options.length)
+    console.log(rnd)
+    console.log(options[rnd])
+    return options[rnd]
+}
